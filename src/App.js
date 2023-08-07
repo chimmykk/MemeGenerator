c6b1afee44eb9a61cd591e2ca58abff9662a5ffd
Yeite Rilsosing Koireng <53212931+chimmykk@users.noreply.github.com>
2023-08-07 20:53:51 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,17 +1,52 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 function ReactAppWithIframe() {
+  const iframeRef = useRef(null);
+
+  // Function to handle messages received from the iframe
+  function handleMessageFromIframe(event) {
+    // Make sure to check the origin to prevent accepting messages from untrusted sources
+    if (event.origin === 'http://localhost:3001') {
+      const { type } = event.data;
+      if (type === 'requestButtonsInfo') {
+        // Get all buttons on the parent page
+        const buttons = Array.from(document.querySelectorAll('button')).map((button) => ({
+          id: button.id,
+          text: button.innerText,
+          disabled: button.disabled,
+        }));
+
+        // Send the buttons information back to the iframe
+        const iframeOrigin = 'http://localhost:3001';
+        const message = { type: 'buttonsInfo', buttons };
+        event.source.postMessage(message, iframeOrigin);
+      }
+    }
+  }
+
+  // Add an event listener to receive messages from the iframe
+  useEffect(() => {
+    window.addEventListener('message', handleMessageFromIframe);
+
+    return () => {
+      // Clean up event listener when component unmounts
+      window.removeEventListener('message', handleMessageFromIframe);
+    };
+  }, []);
+
   return (
     <div>
       <h1>So Webpage is the main parent</h1>
       <div>
-      <iframe title="Webpage X" src="http://localhost:3001/" width="800" height="600"></iframe>
-        <button onClick={() => window.location.href = 'https://www.google.com'}>Go to Google</button>
-        <button onClick={() => window.location.href = 'https://www.facebook.com'}>Go to Facebook</button>
-        <button onClick={() => window.location.href = 'https://www.youtube.com'}>Go to YouTube</button>
+        {/* Add the ref to the iframe */}
+        <iframe ref={iframeRef} title="Webpage X" src="http://localhost:3001/" width="800" height="600"></iframe>
+        {/* Add buttons on the parent page */}
+        <button id="button1">Button testung</button>
+        <button id="button2">Button </button>
+        
+        {/* Add more buttons as needed */}
       </div>
     </div>
-    
   );
 }
 
